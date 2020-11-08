fdb2267c52a1b526847b9dbf9751471ad2282537
Keith Chester <kchester@gmail.com>
2020-11-08 14:31:24 -0800

    feature: error mapping, better handling of calls

--- safestop.go
+++ safestop.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/signal"
 	"sync"
+	"syscall"
 	"time"
 )
 
@@ -16,70 +17,95 @@ type HaltHandler func(ctx context.Context) error
 //HaltHandler functions (in the order of instantiation) upon a registered kill signal coming
 //through. HaltHandler functions will use contexts that may be canceled due to timeouts
 type SafeStop struct {
-	fncs []HaltHandler // collection of HaltHandlers Safestop will call on a signal
+	fncs map[string]HaltHandler // collection of HaltHandlers Safestop will call on a signal
 
 	lock sync.Mutex // We need to ensure that we have atomic access to fncs
 
 	signalsChannel chan os.Signal
 
-	Options *Opts
+	options *Opts
 }
 
 //Opts is the config for a given SafeStop
 type Opts struct {
-	Signals    []os.Signal
-	Timeout    time.Duration
-	OnError    func(error) //OnError is an optional function that is called whenever any handler encounters an error
-	OnComplete func()      //OnComplete is an optional function called when all HaltHandlers are called
+	Signals []os.Signal //defaults to SIGINT and SIGTERM
+	Timeout time.Duration
+
+	//OnError is an optional function that is called whenever
+	//any handler encounters an error. It will consist of the
+	//given identifer for the handler (passed at registry
+	//time) and the error generated.
+	OnError func(identifier string, err error)
+
+	//OnComplete is an optional function called when all
+	//HaltHandlers are called. It will pass a map of each
+	//registered handler and whether it errored out.
+	OnComplete func(errors map[string]error)
 }
 
 //NewSafeStop instantiates a new SafeStop as per options
 func NewSafeStop(opts *Opts) *SafeStop {
 	ss := &SafeStop{
-		fncs:           []HaltHandler{},
+		fncs:           map[string]HaltHandler{},
 		lock:           sync.Mutex{},
 		signalsChannel: make(chan os.Signal),
-		Options:        opts,
+		options:        opts,
 	}
 
-	go func() {
-		<-ss.signalsChannel
+	//Default to SIGINT and SIGTERM
+	if len(ss.options.Signals) == 0 {
+		ss.options.Signals = []os.Signal{syscall.SIGTERM, syscall.SIGINT}
+	}
 
-		ss.lock.Lock()
-		defer ss.lock.Unlock()
+	//Await the signals to come in - this will
+	//hold forever until a registered signal occurs
+	go ss.awaitSignal()
 
-		//Setup a timeout context - all functions should finish within a set time
-		ctx, cancelFunc := context.WithTimeout(context.Background(), ss.Options.Timeout)
-		defer cancelFunc()
-
-		//We have been triggered - begin executing handlers
-		//with timeout
-		for _, handler := range ss.fncs {
-			err := handler(ctx)
-			if err != nil && ss.Options.OnError != nil {
-				ss.Options.OnError(err)
-			}
-		}
-
-		if ss.Options.OnComplete != nil {
-			ss.Options.OnComplete()
-		}
-	}()
-
+	//Now that we can handle incoming signals, register for them
 	ss.registerSignals()
 
 	return ss
 }
 
-//RegisterHandler will add the given HaltHandler for SafeStop to manage
-func (ss *SafeStop) RegisterHandler(fnc HaltHandler) {
+//awaitSignal is the function that handles the triggering from an
+//incoming signal. It is called within the creation of a SafeStop
+//and should not be called again or you will double Handler execs
+func (ss *SafeStop) awaitSignal() {
+	<-ss.signalsChannel
+
 	ss.lock.Lock()
-	ss.fncs = append(ss.fncs, fnc)
+	defer ss.lock.Unlock()
+
+	//Setup a timeout context - all functions should finish within a set time
+	ctx, cancelFunc := context.WithTimeout(context.Background(), ss.options.Timeout)
+	defer cancelFunc()
+
+	errors := map[string]error{}
+
+	//We have been triggered - begin executing handlers
+	//with timeout
+	for id, handler := range ss.fncs {
+		err := handler(ctx)
+		errors[id] = err
+		if err != nil && ss.options.OnError != nil {
+			ss.options.OnError(id, err)
+		}
+	}
+
+	if ss.options.OnComplete != nil {
+		ss.options.OnComplete(errors)
+	}
+}
+
+//RegisterHandler will add the given HaltHandler for SafeStop to manage
+func (ss *SafeStop) RegisterHandler(id string, fnc HaltHandler) {
+	ss.lock.Lock()
+	ss.fncs[id] = fnc
 	ss.lock.Unlock()
 }
 
 //registerSignals internally manages registering for each signal requested
 //and then calls the registered functions upon trigger
 func (ss *SafeStop) registerSignals() {
-	signal.Notify(ss.signalsChannel, ss.Options.Signals...)
+	signal.Notify(ss.signalsChannel, ss.options.Signals...)
 }
